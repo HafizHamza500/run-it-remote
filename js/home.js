9fec122b6a878b8fb9cafe81fc206135d96d57d2
abd-u-rahman <63711139+abd-u-rahman@users.noreply.github.com>
2026-04-22 01:07:39 +0000

    asdf

--- js/home.js
+++ js/home.js
@@ -429,6 +429,8 @@ document.querySelectorAll('.testimonial-video-play').forEach(button => {
   });
 });
 
+
+
 // =================== INITIALIZE ===================
 document.addEventListener('DOMContentLoaded', () => {
   initLenis();
